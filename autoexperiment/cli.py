a5281dadde5c3bfe536640e6c65178fda54aaa23
Mehdi Cherti <m.cherti@fz-juelich.de>
2023-11-16 21:46:44 +0100

    separating sbatch generation part, and launching part

--- autoexperiment/cli.py
+++ autoexperiment/cli.py
@@ -1,5 +1,5 @@
 """Console script for autoexperiment."""
-from clize import run
+from clize import run as clize_run
 import sys
 import os
 from autoexperiment.template import generate_job_defs
@@ -7,20 +7,28 @@ from autoexperiment.manager import manage_jobs_forever
 
 
 def main():
-    run(launch)
+    return clize_run([build, run])
 
-def launch(config):
+
+def build(config, *, exp:str=None):
     if not config:
          print("Please specify a config file")
          return 1
-    jobdefs = generate_job_defs(config)
+    jobdefs = generate_job_defs(config, exp_name=exp)
     for jobdef in jobdefs:
        with open(f"{jobdef.sbatch_script}", "w") as f:
           f.write(jobdef.config)
        os.makedirs(os.path.dirname(jobdef.output_file), exist_ok=True)
+    return 0
+
+def run(config, *, exp:str=None):
+    if not config:
+         print("Please specify a config file")
+         return 1
+    jobdefs = generate_job_defs(config, exp_name=exp)
     manage_jobs_forever(jobdefs)
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(run(main))  # pragma: no cover
+    sys.exit(main())  # pragma: no cover
